b5d1dfc1946521fce5bd53df437e622a25a4d73e
PhdTrollSlayer <rodrigomauricio@protonmail.com>
2019-03-04 18:45:34 -0300

    90%

--- src/main.rs
+++ src/main.rs
@@ -53,11 +53,12 @@ impl Aviao {
         }
     }
 
-    fn levantar_voo(&mut self, destino: String, l_aero: &mut HashMap<String, Aeroporto>) -> Result<(), ()> {
+    fn levantar_voo(&mut self, destino: String) -> Result<String, ()> {
+        let mut s = String::new();
         self.estado = match &self.estado {
-            AviaoEstado::Estacionado {tempo_saida, local} => {
-                let mut a = l_aero.get_mut(local).unwrap();
-                a.pistas += 1;
+            AviaoEstado::Estacionado {tempo_saida: _, local} => {
+
+                s = local.clone();
                 AviaoEstado::Voando{
                     tempo_chegada: time::Duration::new(0, 0),
                     combustivel_sobra: time::Duration::new(0, 0),
@@ -67,13 +68,14 @@ impl Aviao {
             _ => return Err(())
         };
 
-        return Ok(())
+        return Ok(s);
+
     }
 
     fn pousar(&mut self, a: String, l_aero: &mut HashMap<String, Aeroporto>) -> Result<(), ()> {
         let mut a = l_aero.get_mut(&a).unwrap();
 
-        if a.pistas < 1 {
+        if a.pistas == 0 {
             self.estado = match self.estado {
                 AviaoEstado::Voando {..} => AviaoEstado::EsperandoAr{
                     combustivel_sobra: time::Duration::new(0, 0),
@@ -100,9 +102,42 @@ impl Aviao {
 
     fn get_destino(&self) -> Result<String, ()> {
         match &self.estado {
-            AviaoEstado::Voando {destino, tempo_chegada: _, combustivel_sobra: _} => {return Ok(destino.clone())},
+            AviaoEstado::Voando {destino, tempo_chegada: _, combustivel_sobra: _} => {
+                return Ok(destino.clone())
+            },
             _ => {return Err(())}
         }
+    }
+
+    fn get_local(&self) -> String {
+        match &self.estado {
+            AviaoEstado::Estacionado {local, tempo_saida: _} => {
+                return local.clone()
+            },
+            _ => {String::new()}
+        }
+    }
+
+}
+
+fn check_destino(local: &String, lst: &HashMap<String, Aeroporto>) -> Result<String, ()> {
+    let mut rng = thread_rng();
+
+    loop {
+        let i: i8 = rng.gen_range(0, lst.len() as i8);
+        let mut dest = String::new();
+
+        for (p, l) in lst.iter().enumerate() {
+            if p == i as usize {
+                dest = l.0.to_string();
+
+            }
+        }
+
+        if !dest.eq(local) {
+            return Ok(dest)
+        }
+
     }
 }
 
@@ -123,57 +158,46 @@ fn main() {
     lista_aviao.push(maq2);
     lista_aviao.push(maq3);
 
-    lista_aviao[0].levantar_voo(aero.nome.clone(), &mut lista_aeroportos).unwrap();
-    lista_aviao[1].levantar_voo(aero.nome.clone(), &mut lista_aeroportos).unwrap();
-    lista_aviao[2].levantar_voo(aero.nome.clone(), &mut lista_aeroportos).unwrap();
+    lista_aviao[0].levantar_voo(String::from("Guarulhos"));
+    lista_aviao[1].levantar_voo(String::from("Guarulhos"));
+    lista_aviao[2].levantar_voo(String::from("Congonhas"));
 
-    let turno = time::Duration::new(1, 0);
+    let turno = time::Duration::new(2, 0);
+
     loop {
-        let mut rng = thread_rng();
 
         sleep(turno);
 
         for a in lista_aviao.iter_mut() {
-            // gera ação
-            let u: i8 = rng.gen_range(0, 2);
+            let dest = check_destino(&a.get_local(), &lista_aeroportos).unwrap();
 
-            // gera destino
-            let i: i8 = rng.gen_range(0, lista_aeroportos.len() as i8);
-            let mut dest = String::new();
-
-            for (p, l) in lista_aeroportos.iter().enumerate() {
-                if p == i as usize {
-                    dest = l.0.to_string();
-
-                }
-            }
-
-            match u {
-                // levantar voo
-                0 => {
-                    match a.levantar_voo(dest, &mut lista_aeroportos) {
-                        Ok(()) => {},
-                        Err(()) => {}
-                    }
+            match a.levantar_voo(dest) {
+                Ok(s) => {
+                    let mut a = lista_aeroportos.get_mut(&s).unwrap();
+                    a.pistas += 1;
+                    assert!(a.pistas < 3);
                 },
-                // pousar
-                1 => {
+                Err(()) => {
                     match a.get_destino() {
                         Ok(q) => {
                             match a.pousar(q, &mut lista_aeroportos) {
                                 Ok(()) => {},
-                                Err(()) => {}
+                                Err(()) => {
+
+                                }
                             }
                         }
-                        Err(_) => {}
-                    };
-                },
-                _ => {}
+                        Err(_) => {
+                        }
+                    }
+
+                }
             }
 
         }
 
-        dbg!(&lista_aviao);
+        //dbg!(&lista_aviao);
+        dbg!(&lista_aeroportos);
 
     }
 
